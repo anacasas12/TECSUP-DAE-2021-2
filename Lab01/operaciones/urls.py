9088fb1d58c9cc2ee8ef3289d10edfcf78d123e1
anacasas12 <76541394+anacasas12@users.noreply.github.com>
2021-12-03 12:48:07 -0500

    Delete Lab01 directory

--- Lab01/operaciones/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns=[
-    path('suma/<int:a>/<int:b>',views.suma,name='suma'),
-    path('resta/<int:a>/<int:b>',views.resta,name='resta'),
-    path('multiplicacion/<int:a>/<int:b>', views.multiplicacion,name='multiplicacion'),
-    path('division/<int:a>/<int:b>',views.division,name='division')
-]
